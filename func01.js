c84f72e78bfc21a7dd5cf01049dd4765729e076f
Khunaiko <pasuaiko21089@gmail.com>
2023-11-07 15:21:07 +0700

    first commit

---
+++ func01.js
@@ -0,0 +1,8 @@
+//Define function
+function myFunction() {
+    console.log("My Firsy Function");
+}
+//Call function
+ myFunction();
+
+document.getElementById("func1").innerHTML = "My Firsy Function";
